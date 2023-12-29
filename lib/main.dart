fc18c3d414de074cd7a29596bae1676584b0bdbd
Narazaka <info@narazaka.net>
2023-12-29 13:57:59 +0900

    sidebar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sidebarx/sidebarx.dart';
 
 void main() {
   runApp(const MyApp());
@@ -56,6 +57,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  SidebarXController controller = SidebarXController(
+    selectedIndex: 0,
+    extended: true,
+  );
 
   void _incrementCounter() {
     setState(() {
@@ -77,43 +82,44 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Row(
+        children: [
+          SidebarX(
+            controller: controller,
+            showToggleButton: false,
+            items: const [
+              SidebarXItem(icon: Icons.dashboard, label: "Projects"),
+              SidebarXItem(icon: Icons.help_center, label: "Learn"),
+              SidebarXItem(icon: Icons.home_repair_service, label: "Tools"),
+            ],
+            footerItems: const [
+              SidebarXItem(icon: Icons.description, label: "Logs"),
+              SidebarXItem(icon: Icons.settings, label: "Settings"),
+            ],
+            theme: const SidebarXTheme(
+              margin: EdgeInsets.all(10),
+              itemTextPadding: EdgeInsets.only(left: 10),
+              selectedItemTextPadding: EdgeInsets.only(left: 10),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+            extendedTheme:
+                const SidebarXTheme(width: 150, margin: EdgeInsets.all(10)),
+          ),
+          Expanded(
+            child: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times!!:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
+            )),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
